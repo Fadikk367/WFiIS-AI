e6b40a50b579dfd59d460276c0d0fd8c4c2cb939
Adrian Furman <fadikk367@gmail.com>
2020-05-30 23:35:17 +0200

    improve dijkstra and primes

--- dijkstra/dijkstra.py
+++ dijkstra/dijkstra.py
@@ -1,11 +1,21 @@
 import json
 from math import inf
 
+def load_graph_from_json(filename):
+    with open(filename, 'r') as f:
+        data = json.load(f)
+
+    graph = {}
+    for item in data:
+        graph[item['name']] = item['neighbours']
+
+    return graph
+
+
 def get_closest_vertex(result):
     unvisited = [{"name": vertex, "distance": details['distance']} for vertex, details in  result.items() if not details['visited']]
-    print(unvisited)
-    closest = unvisited[0]
 
+    closest = unvisited[0]
     for i in range(1, len(unvisited) - 1):
         if unvisited[i]['distance'] < closest['distance']:
             closest = unvisited[i]
@@ -13,23 +23,18 @@ def get_closest_vertex(result):
     return closest['name']
 
 
-
 def shortest_path(graph, start_vertex):
     result = {}
-    visited = []
-    unvisited = []
     for vertex in graph:
         result[vertex] = {'distance': inf, 'previous': None, 'visited': False}
-        unvisited.append(vertex)
+
 
     result[start_vertex]['distance'] = 0
   
     not_empty = True
 
     while not_empty:
-        print(not_empty)
         current = get_closest_vertex(result)
-
         result[current]['visited'] = True
 
         for neighbour in graph[current]:
@@ -40,24 +45,32 @@ def shortest_path(graph, start_vertex):
             
         not_empty = any([not result[vertex]['visited'] for vertex in result])
 
-    for item in result:
-        print(f"{item}: {result[item]}")
+    return result
 
 
+def get_path_as_string(result, start, destination):
+    path = []
+    current = destination
+    while current != start:
+        section = {
+            'distance': result[current]['distance'] - result[result[current]['previous']]['distance'],
+            'name': current
+        }
+        path.insert(0, section)
+        current = result[current]['previous']
 
-        
-            
+    path_str = f'{start} '
+    for section in path:
+        path_str += f'=> {section["name"]}({section["distance"]}km) '
+    path_str += f'; total: {result[destination]["distance"]}km'
 
+    return path_str
 
-graph = {}
+    
 
-
-with open('cities.json', 'r') as f:
-    data = json.load(f)
-
-    for item in data:
-        graph[item['name']] = item['neighbours']
-
-
-shortest_path(graph, "Arad")
+if __name__ == '__main__':
+    graph = load_graph_from_json('cities.json')
+    result = shortest_path(graph, start_vertex="Arad")
+    path = get_path_as_string(result, start="Arad", destination="Bucharest")
+    print(path)
 
--- primes/primes.cpp
+++ primes/primes.cpp
@@ -1,20 +1,40 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
 #include <fstream>
 #include <chrono>
+#include <exception>
 
 enum class PrimesAlgorithm { BRUTE_FORCE,  ERATOSTENES_SIEVE, ATKIN_SIEVE };
 
 class PrimesGenerator {
     public:
-    PrimesGenerator(int n, PrimesAlgorithm algorithm = PrimesAlgorithm::ERATOSTENES_SIEVE) 
-        : 
-        m_n(n), 
-        m_algoritm(algorithm),
-        m_primes(new bool[n])
+    PrimesGenerator(int n = 0, PrimesAlgorithm algorithm = PrimesAlgorithm::ERATOSTENES_SIEVE) 
+        try : 
+        m_n(n),
+        m_algoritm(algorithm)
     {
+        if (n <= 0)
+            throw std::invalid_argument("Only positive numbers allowed! \n * wrong parameter has been given \n * int32 overflow might have occured \n * invalid argument might have been given (not convertable to int)");
+        if (n == 1)
+            throw "no primes within this range";
+
+        m_primes = new bool[m_n];
         for (int i = 0; i < n; ++i)
             m_primes[i] = true;
+    }
+    catch (std::invalid_argument& e) {
+        std::cout << "ERROR: Invalid constructor parameters!" << std::endl;
+        throw; 
+    }
+    catch (const char* msg) {
+        throw;
+    }
+
+    static void info() {
+        std::cout << "\nThis is simple prime numbers generator" << std::endl;
+        std::cout << "Upper bound of numbers within generator can look for primes is the size of 4 byte int ~ 2,147,483,647" << std::endl;
+        std::cout << "By default floating point numbers are casted to integers\n" << std::endl;
     }
 
     void setAlgorithm(PrimesAlgorithm algorithm) {
@@ -39,12 +59,14 @@ class PrimesGenerator {
     }
 
     void saveToFile(std::string filename) {
+        std::cout << "<=== Saving results to file: \""<< filename << "\" ... ===>" << std::endl;
         std::ofstream resultStream{filename, std::ios::out};
 
-        for (int i = 1; i <= m_n; ++i)
+        for (int i = 2; i <= m_n; ++i)
             if (m_primes[i]) resultStream << i << std::endl;
 
         resultStream.close();
+        std::cout << "<=== finished ===>" << std::endl;
     }
 
     ~PrimesGenerator() {
@@ -80,12 +102,20 @@ class PrimesGenerator {
 
 
 int main(int argc, char* argv[]) {
+    PrimesGenerator::info();
     int n = atoi(argv[1]);
     std::cout << "Generate primes up to: " << n << std::endl;
-    PrimesGenerator generator(n);
-    generator.setAlgorithm(PrimesAlgorithm::ERATOSTENES_SIEVE);
-
-    generator.run();
-    // generator.saveToFile("out.txt");
+    try {
+        PrimesGenerator generator(n);
+        generator.setAlgorithm(PrimesAlgorithm::ERATOSTENES_SIEVE);
+        generator.run();
+        generator.saveToFile("out.txt");
+    }
+    catch(std::invalid_argument& e) {
+        std::cout << e.what() << std::endl;
+    }
+    catch(const char* msg) {
+        std::cout << msg << std::endl;
+    }
     return 0;
 }
